048d30950ff803903acdb1dab1a1fa2d9236961c
Parsa Sharifi <sharifidev@outlook.com>
2023-08-29 20:00:50 +0330

    add some file to .gitignore

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '/utils/config.dart';
+import '/utils/text.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -11,8 +13,36 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Auth Page!'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15,
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppText.enText['welcome_text']!,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaseSmall,
+              Text(
+                AppText.enText['signIn_text']!,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaseSmall,
+              // Login component
+            ],
+          ),
+        ),
       ),
     );
   }
